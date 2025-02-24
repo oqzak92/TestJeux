dafc3a3ae0dbd0240783fd311ed34b5cb5621a7c
oqzak92 <oqzak92@gmail.com>
2025-02-24 12:00:52 +0100

    Message de commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Voiture from './Composant/Voiture.jsx'
 import Voiture2 from './Composant/Voiture2.jsx'
+import Voiture3 from './Composant/Voiture3.jsx'
 
 function App() {
 
   return (
-    <Voiture2 />
+    <Voiture3 />
   )
 
 
